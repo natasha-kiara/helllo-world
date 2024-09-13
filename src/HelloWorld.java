59e7f5c52756c8ff6579054d8d9c6cc46a24d5b8
The-shark <natashankatha@gmail.com>
2024-09-13 16:34:45 -0400

    hello world project

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kiara
+ */
+public class HelloWorld {
+// psvm-> tab, sout->tab to print   
+    public static void main(String[] args) {
+        System.out.println("Hello World");  
+    }
+}
